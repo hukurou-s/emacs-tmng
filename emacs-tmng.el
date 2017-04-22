ff81be5674a74d4b0bf2bdd083d67b8a40602271
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2017-04-23 02:32:34 +0900

    {}に色がつくようになった

--- emacs-tmng.el
+++ emacs-tmng.el
@@ -4,6 +4,10 @@
   (kill-all-local-variables)
   (setq mode-name "tmng-mode")
   (setq major-mode 'tmng-mode)
+  (set-syntax-table tmng-mode-syntax-table)
+  (set (make-local-variable 'font-lock-defaults) '(tmng-mode-font-lock-keywords))
+  (run-hooks 'wpdl-mode-hook))
+
 
   (defvar tmng-mode-syntax-table
     (let ((st (make-syntax-table)))
@@ -13,8 +17,9 @@
     "Syntax table for `tmng-mode'.")
   
 
-  (defvar tmng-mode-font-lock-keywords
-    '(" ([0-9]+)" . font-lock-constant-face)
-       "Keyword highlighting specification for `tmng-mode'."))
+  (defconst tmng-mode-font-lock-keywords (list
+;    '("\\ {'\\w*'\\}" . font-lock-variable-name-face)
+    '("{}" . font-lock-variable-name-face))
+    "Minimal highlighting expressions for Tmng mode")
 
 (provide 'emacs-tmng)
