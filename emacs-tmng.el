4868e2ddaad6a1eb57d10a4129743ce3cbad0f04
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2017-04-25 22:53:32 +0900

    日付に色がつく正規表現の追加と項目によって違う色がつくように修正

--- emacs-tmng.el
+++ emacs-tmng.el
@@ -6,28 +6,32 @@
   (setq major-mode 'tmng-mode)
   (set-syntax-table tmng-mode-syntax-table)
   (set (make-local-variable 'font-lock-defaults) '(tmng-mode-font-lock-keywords))
-  (run-hooks 'wpdl-mode-hook))
+  (run-hooks 'tmng-mode-hook))
 
 
-  (defvar tmng-mode-syntax-table
-    (let ((st (make-syntax-table)))
-      (modify-syntax-entry ?# "<" st)
-      (modify-syntax-entry ?\n ">" st)
-      st)
-    "Syntax table for `tmng-mode'.")
+(defvar tmng-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?# "<" st)
+    (modify-syntax-entry ?\n ">" st)
+    st)
+  "Syntax table for `tmng-mode'.")
   
 
-  (defconst tmng-mode-font-lock-keywords (list
-    '("{.*}" . font-lock-variable-name-face)
-    '("\\[.*\\]" . font-lock-variable-name-face)
-    '("(.*)" . font-lock-variable-name-face)
-    '("■.*" . font-lock-variable-name-face)
-    '("□.*" . font-lock-variable-name-face)
-    '("●.*" . font-lock-variable-name-face)
-    '("◎.*" . font-lock-variable-name-face)
-    '("○.*" . font-lock-variable-name-face))
+(defconst tmng-mode-font-lock-keywords
+  (list
+   '("[0-9]\\{4\\}\\.[0-9]\\{2\\}\\.[0-9]\\{2\\}(\\(月\\|火\\|水\\|木\\|金\\|土\\|日\\|__\\))" . font-lock-function-name-face)
+   '("(.*)" . font-lock-variable-name-face)
+   '("{.*}" . font-lock-variable-name-face)
+   '("\\[.*\\]" . font-lock-variable-name-face)
+   '("■.*" . font-lock-string-face)
+   '("□.*" . font-lock-string-face)
+   '("●.*" . font-lock-string-face)
+   '("◎.*" . font-lock-string-face)
+   '("○.*" . font-lock-string-face)
+   '("\\(研究会合\\|月例発表\\)" . font-lock-string-face)
+   )
 
+  "Minimal highlighting expressions for Tmng mode")
 
-    "Minimal highlighting expressions for Tmng mode")
 
 (provide 'emacs-tmng)
