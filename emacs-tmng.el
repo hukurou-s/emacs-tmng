1ee5677b51339a66d0a49a728b31c8d033f40e52
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2017-04-27 23:58:12 +0900

    正規表現を修正

--- emacs-tmng.el
+++ emacs-tmng.el
@@ -15,23 +15,30 @@
     (modify-syntax-entry ?\n ">" st)
     st)
   "Syntax table for `tmng-mode'.")
-  
+
+(defvar font-lock-defaults
+  (list
+   'tmng-mode-font-lock-keywords
+   nil ; keywords-only = nil
+   nil ; case-fold = nil i.e. case-sensitiveh
+   nil ; syntax-alist
+   'beginning-of-line
+   ))
 
 (defconst tmng-mode-font-lock-keywords
-  (list
-   '("[0-9]\\{4\\}\\.[0-9]\\{2\\}\\.[0-9]\\{2\\}(\\(月\\|火\\|水\\|木\\|金\\|土\\|日\\|__\\))" . font-lock-function-name-face)
-   '("(.*)" . font-lock-variable-name-face)
-   '("{.*}" . font-lock-variable-name-face)
-   '("\\[.*\\]" . font-lock-variable-name-face)
-   '("■.*" . font-lock-string-face)
-   '("□.*" . font-lock-string-face)
-   '("●.*" . font-lock-string-face)
-   '("◎.*" . font-lock-string-face)
-   '("○.*" . font-lock-string-face)
-   '("\\(研究会合\\|月例発表\\)" . font-lock-string-face)
-   )
-
+   '(
+     ("[0-9]\\{4\\}\\.[0-9]\\{2\\}\\.[0-9]\\{2\\}(\\(月\\|火\\|水\\|木\\|金\\|土\\|日\\|__\\))" . font-lock-function-name-face)
+     ("(.*)" . font-lock-variable-name-face)
+     ("{.*}" . font-lock-variable-name-face)
+     ("\\[.*\\]" . font-lock-variable-name-face)
+     ("■.*" . font-lock-string-face)
+     ("□.*" . font-lock-string-face)
+     ("●.*" . font-lock-string-face)
+     ("◎.*" . font-lock-string-face)
+     ("○.*" . font-lock-string-face)
+     ("\\(研究会合\\|月例発表\\|定例会合\\|個人作業\\|委託業務\\|定例活動\\)" . font-lock-keyword-face)
+     )
+  
   "Minimal highlighting expressions for Tmng mode")
-
 
 (provide 'emacs-tmng)
