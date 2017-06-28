46c70b06c2ecbd19fc5a6bb101f2d16f19011040
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2017-06-28 16:31:01 +0900

    色をつける優先順位と条件を修正。かなり良くなった気がする。

--- emacs-tmng.el
+++ emacs-tmng.el
@@ -1,5 +1,3 @@
-(require 'cl)
-
 (defun tmng-mode ()
   "Tmng Mode "
   (interactive)
@@ -21,11 +19,13 @@
 (defconst tmng-mode-font-lock-keywords-1
   	  (list
 	   '("[0-9]\\{4\\}\\.[0-9]\\{2\\}\\.[0-9]\\{2\\}(\\(月\\|火\\|水\\|木\\|金\\|土\\|日\\|__\\))" . font-lock-function-name-face)
-	   '("■.*" . font-lock-string-face)
-	   '("□.*" . font-lock-string-face)
-	   '("●.*" . font-lock-string-face)
-	   '("◎.*" . font-lock-string-face)
-	   '("○.*" . font-lock-string-face)
+	   '("[0-9]\\{2\\}\\.[0-9]\\{2\\}(\\(月\\|火\\|水\\|木\\|金\\|土\\|日\\|__\\))" . font-lock-function-name-face)
+	   '("[0-9]\\{2\\}:[0-9]\\{2\\}-[0-9]\\{2\\}:[0-9]\\{2\\}" . font-lock-function-name-face)
+	   '("(.*)" . font-lock-variable-name-face)
+	   '("{.*}" . font-lock-variable-name-face)
+	   '("<.*>" . font-lock-variable-name-face)
+	   '("\\[.*\\]" . font-lock-variable-name-face)
+	   ;'("\\((\\|{\\|<\\|\\[\\).*\\()\\|}\\|>\\|\\]\\)" . font-lock-string-face)
 	   ;'("\\(研究会合\\|月例発表\\|定例会合\\|個人作業\\|委託業務\\|定例活動\\)" . font-lock-keyword-face)
 	   )
 
@@ -35,12 +35,10 @@
 (defconst tmng-mode-font-lock-keywords-2
   (append tmng-mode-font-lock-keywords-1
 	  (list
-	   '("(.*)" . font-lock-variable-name-face)
-	   '("{.*}" . font-lock-variable-name-face)
-	   '("<.*>" . font-lock-variable-name-face)
-	   '("\\[.*\\]" . font-lock-variable-name-face)
-	   '("[0-9]\\{2\\}:[0-9]\\{2\\}-[0-9]\\{2\\}:[0-9]\\{2\\}" . font-lock-function-name-face)
-	   '("\\(研究会合\\|月例発表\\|定例会合\\|個人作業\\|委託業務\\|定例活動\\)" . font-lock-keyword-face)
+	   '("\\(月例発表\\)" . font-lock-warning-face)
+	   '("\\(定例会合\\|個人作業\\|委託業務\\|定例活動\\|週間予定\\|個人報告\\|週直担当\\|研究会合\\)" . font-lock-keyword-face)
+	   '("\\(■\\|□\\|●\\|◎\\|○\\).*" . font-lock-string-face)
+	   '("\\(■\\|□\\|●\\|◎\\|○\\)" . font-lock-string-face)
 	   ))
   
    "Minimal highlighting expressions for Tmng mode")
